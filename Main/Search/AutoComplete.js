08df2185e041f654e5eeeed078662cc931ddf69d
Bodicj <Bodicj@gmail.com>
2016-10-31 01:26:27 +0200

    Added auto creation of div's
    Auto creation without auto cmpletion from json

---
+++ Main/Search/AutoComplete.js
@@ -0,0 +1,14 @@
+$(document).ready( function Search () {
+    var n = 10;
+    var div;
+    var ContainerName = "Container";
+    for(i = 0; i < n; i++){
+        div = document.createElement('div');
+        div.className = "Catalog";
+        var NumContainer = i + 1;    
+        div.innerHTML = "<strong>Ура!</strong> Вы прочитали это важное сообщение.";
+        document.getElementById(ContainerName).appendChild(div);
+        document.getElementById(ContainerName).id = "Container"+NumContainer.toString(); 
+        ContainerName = "Container"+NumContainer.toString();
+        }
+});
